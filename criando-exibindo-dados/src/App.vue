a6ac9c65ff9c4ca11ac466297cd55f3c2f8275af
lazarodaniel83 <ldss158@gmail.com>
2022-02-03 21:21:25 -0300

    livro vue cap 4 finalizado

---
+++ criando-exibindo-dados/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <h1 v-text="nome"></h1>
+    <small v-if="mostra" v-once v-text="nome"></small>
+
+    <button @click="mostra = !mostra" :disabled="nome != 'Mineirão'">Trocar</button>
+
+    <input type="text" v-model="nome">
+
+    <ul>
+      <li v-for="qnt in cadeiras">{{ qnt }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      nome: 'Mané Garrincha',
+      mostra : true,
+      cadeiras: [
+        '30.000 brancas',
+        '30.000 cinzas',
+        '10.000 vips',
+      ]
+    }
+  },
+  mounted() {
+    setTimeout(() => this.nome = 'Mineirão', 4000)
+  }
+}
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+ }
+
+ h1, h2 {
+  font-weight: normal;
+ }
+
+ ul {
+  list-style-type: none;
+  padding: 0;
+ }
+
+ li {
+  display: inline-block;
+  margin: 0 10px;
+ }
+
+ a {
+  color: #42b983;
+ }
+</style>
+
+
+
+
+
---
+++ helloworld/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render (h){
+    return (
+      <template>
+        <div>
+          <button>Entrar</button>
+        </div>
+      </template>
+    )
+  }
+})
